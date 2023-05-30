c2c6fd6546436c347aa574be738d585dbae35ebc
Medmalek98 <95756360+Medmalek98@users.noreply.github.com>
2023-05-30 16:55:12 +0100

    Update IJobService.java

--- src/main/java/com/esprit/gestioncondidat/service/IJobService.java
+++ src/main/java/com/esprit/gestioncondidat/service/IJobService.java
@@ -5,6 +5,7 @@ import com.esprit.gestioncondidat.entity.Job;
 import java.util.List;
 import java.util.Optional;
 
+
 public interface IJobService {
     void addJobs(Job job);
     List<Job> afficherTousJobs();
