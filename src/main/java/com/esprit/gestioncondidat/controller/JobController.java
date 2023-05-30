1ac5f24a50d2c8a1f8ef5a86425e7b2c329fe597
Medmalek98 <95756360+Medmalek98@users.noreply.github.com>
2023-05-30 16:53:57 +0100

    Update JobController.java

--- src/main/java/com/esprit/gestioncondidat/controller/JobController.java
+++ src/main/java/com/esprit/gestioncondidat/controller/JobController.java
@@ -4,7 +4,6 @@ import com.esprit.gestioncondidat.entity.Job;
 import com.esprit.gestioncondidat.service.JobServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 @RequestMapping("/apispring")
