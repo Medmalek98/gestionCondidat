4920e6f015ca66c1741f64b7b3a7268c0e239523
Medmalek98 <95756360+Medmalek98@users.noreply.github.com>
2023-05-30 16:53:45 +0100

    Update CandidatController.java

--- src/main/java/com/esprit/gestioncondidat/controller/CandidatController.java
+++ src/main/java/com/esprit/gestioncondidat/controller/CandidatController.java
@@ -4,7 +4,6 @@ import com.esprit.gestioncondidat.entity.Candidat;
 import com.esprit.gestioncondidat.service.CandidatServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RequestMapping("/apispring")
